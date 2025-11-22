c346b11eb2481bfc4272eaebbcbd47c872175330
taiki yamaguchi <taikiyamaguchi@taikinoMacBook-Air.local>
2025-11-22 15:37:00 +0900

    remove prisma migrations for deployment

--- prisma/migrations/20251025063326_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Employee" (
-    "employee_id" TEXT NOT NULL PRIMARY KEY,
-    "employee_name" TEXT NOT NULL,
-    "license" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
